fab5f3249a260ea4d8656f221d2c76c11c512b2f
thuydx98 <thuydx.9598@gmail.com>
2021-08-12 12:07:29 +0700

    Add more features for search options

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
 import { InspectionService } from './inspection.service';
 import { TransferItem } from 'ng-zorro-antd/transfer';
+import { NzModalService } from 'ng-zorro-antd/modal';
 import * as lunr from 'lunr';
+import { SearchOption } from './models/app.model';
+import { getSimilarityBasedOnDistance, searchExactly } from './app.const';
 
 @Component({
   selector: 'app-root',
@@ -16,8 +19,9 @@ export class AppComponent {
   rawInspection: any[] = [];
   inspections: any[] = [];
   idx: any;
+  options: SearchOption = new SearchOption();
 
-  constructor(private appSettingsService: InspectionService) {}
+  constructor(private appSettingsService: InspectionService, private modal: NzModalService) {}
 
   ngOnInit() {
     this.appSettingsService.get().subscribe((data) => {
@@ -49,6 +53,7 @@ export class AppComponent {
     this.timeoutID = setTimeout(() => {
       this.inspections = this.rawInspection;
       this.generateSearchPatterns(this.searchKey);
+      this.onSearch();
     }, 500);
   }
 
@@ -57,6 +62,110 @@ export class AppComponent {
     this.patterns = [];
     this.inspections = this.rawInspection;
     this.loading = false;
+  }
+
+  onSearch(isChangeScore = false): void {
+    this.loading = true;
+    const patterns = this.patterns.filter((item) => item.direction === 'right');
+
+    if (!this.searchKey || (patterns.length == 0 && !this.options.useCustomSearch)) {
+      this.inspections = this.rawInspection;
+      this.loading = false;
+      return;
+    }
+
+    let result: any = [];
+    const searchKey = this.searchKey?.trim();
+
+    if (this.options.useCustomSearch) {
+      const isSingle = searchKey.split(' ').length === 1;
+      result = result.concat(searchExactly(searchKey, this.rawInspection, isSingle).map((item: any) => ({ ...item, tooltip: 'CSE' })));
+    }
+
+    patterns.forEach((pattern) => {
+      const tooltip = pattern.title;
+      try {
+        result = result.concat(this.idx.search(tooltip).map((item: any) => ({ ...item, tooltip: pattern.title })));
+      } catch (error) {
+        this.modal.error({
+          nzTitle: 'Pattern format error',
+          nzContent: error.name + ': ' + error.message,
+        });
+      }
+    });
+
+    this.inspections = result
+      .filter((value: any, index: any, self: any) => self.findIndex((m: any) => m.ref === value.ref) === index)
+      .map((item: any) => {
+        const insp = this.rawInspection.find((ins) => ins.inspectionId === item.ref);
+
+        const inspection = Object.assign({}, insp);
+        if (inspection) {
+          inspection.condition = item.condition;
+          for (const [key] of Object.entries(item.matchData.metadata)) {
+            inspection.inspectionLineName = inspection.inspectionLineName.replace(new RegExp(key, 'i'), `<b>$&</b>`);
+          }
+
+          inspection.score = +item.score.toFixed(3);
+          inspection.inspectionLineName = `<span class="text-danger">${item.score === 100 ? 'CSE' : item.score.toFixed(3)}</span>: ` + inspection.inspectionLineName;
+        }
+
+        return inspection;
+      });
+
+    if (this.options.useLevenshtein) {
+      this.inspections = this.inspections.sort((ins1: any, ins2: any) => {
+        let diff = ins2.score - ins1.score;
+        if (diff === 0) {
+          diff = getSimilarityBasedOnDistance(this.searchKey, ins2.inspectionLineName) - getSimilarityBasedOnDistance(this.searchKey, ins1.inspectionLineName);
+        }
+        return diff;
+      });
+    }
+
+    if (!isChangeScore) {
+      const minScore = this.inspections.reduce((min, item) => (min < item.score ? min : item.score), 0);
+      const maxScore = this.inspections.reduce((max, item) => (max > item.score ? max : item.score), 0);
+      this.options.scoreRange = [minScore, maxScore];
+      this.options.scoreSelected = [minScore, maxScore];
+    } else {
+      this.inspections = this.inspections.filter((item) => this.options.scoreSelected[0] <= item.score && item.score <= this.options.scoreSelected[1]);
+    }
+
+    if (this.options.isSortScore) {
+      this.inspections = this.inspections.sort((a, b) => (a.score <= b.score && 1) || -1);
+    }
+
+    this.loading = false;
+  }
+
+  onAddPattern(direction: any): void {
+    const pattern = prompt('Please exter the pattern:', '');
+    if (pattern) {
+      this.patterns = [
+        ...this.patterns,
+        {
+          title: pattern || '',
+          direction,
+        },
+      ];
+
+      if (direction === 'right') this.onSearch();
+    }
+  }
+
+  onDeletePattern(pattern: any): void {
+    this.modal.confirm({
+      nzTitle: `Are you sure delete this pattern ${pattern.title}?`,
+      nzOkText: 'Yes',
+      nzOkType: 'primary',
+      nzOkDanger: true,
+      nzOnOk: () => {
+        this.patterns = this.patterns.filter((item) => item.title !== pattern.title);
+        this.onSearch();
+      },
+      nzCancelText: 'No',
+    });
   }
 
   private generateSearchPatterns(searchKey: string): void {
@@ -79,52 +188,14 @@ export class AppComponent {
       patterns.push({ title: '*' + searchKey + '*' });
       patterns.push({ title: [...searchKey].join('*') + '*' });
     } else {
-      patterns.push({ title: searchKey + '*' });
       patterns.push({ title: '+' + searchKey.split(' ').join(' +') });
       patterns.push({ title: '+' + searchKey.split(' ').join(' +') + '*' });
       patterns.push({ title: searchKey.split(' ').join('* ') + '*' });
+      patterns.push({ title: searchKey + '*' });
     }
 
     this.patterns = patterns;
   }
-
-  private onSearch = () => {
-    this.loading = true;
-
-    if (this.patterns.length == 0) {
-      this.loading = false;
-      this.inspections = this.rawInspection;
-      return;
-    }
-
-    let result: any = [];
-    const patterns = this.patterns.filter((item) => item.direction === 'right');
-
-    patterns.forEach((pattern) => {
-      const tooltip = pattern.title;
-      result = result.concat(this.idx.search(tooltip).map((item: any) => ({ ...item, tooltip: pattern.title })));
-    });
-
-    this.inspections = result
-      .filter((value: any, index: any, self: any) => self.findIndex((m: any) => m.ref === value.ref) === index)
-      .map((item: any) => {
-        const insp = this.rawInspection.find((ins) => ins.inspectionId === item.ref);
-
-        const inspection = Object.assign({}, insp);
-        if (inspection) {
-          inspection.condition = item.condition;
-          for (const [key] of Object.entries(item.matchData.metadata)) {
-            inspection.inspectionLineName = inspection.inspectionLineName.replace(new RegExp(key, 'i'), `<b>$&</b>`);
-          }
-
-          inspection.inspectionLineName = `<span class="text-danger">${item.score === 100 ? 'CSE' : item.score.toFixed(3)}</span>: ` + inspection.inspectionLineName;
-        }
-
-        return inspection;
-      });
-
-    this.loading = false;
-  };
 
   private initLunrIndex(inspections: any): void {
     const trimmer = function (token: any) {
---
+++ src/app/models/app.model.ts
@@ -0,0 +1,7 @@
+export class SearchOption {
+  public useLevenshtein = false;
+  public useCustomSearch = false;
+  public isSortScore = false;
+  public scoreRange = [0, 3];
+  public scoreSelected = [0.5, 1];
+}
